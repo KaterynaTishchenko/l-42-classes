a66814259b944df9cdae743478358290b7be6729
KaterynaTishchenko <111631799+KaterynaTishchenko@users.noreply.github.com>
2023-05-03 14:14:44 +0300

    adding task 2

--- main.js
+++ main.js
@@ -39,4 +39,71 @@ class Circle {
     const result = `Радіус: ${circle.radius}, Діаметр: ${circle.diameter}, Площа: ${circle.area}, Довжина: ${circle.circumference}`;
     resultParagraph.textContent = result;
   });
-
+  class Marker {
+    constructor(color, inkLevel) {
+      this.color = color;
+      this.inkLevel = inkLevel;
+    }
+  
+    write(text) {
+      const inkPerChar = 0.5;
+      let inkUsed = 0;
+      for (let i = 0; i < text.length; i++) {
+        if (text[i] !== " ") {
+          inkUsed += inkPerChar;
+        }
+        if (inkUsed > this.inkLevel) {
+          alert("Маркер закінчився!");
+          break;
+        }
+        alert(`%c${text[i]}`, `color: ${this.color}`);
+      }
+      this.inkLevel -= inkUsed;
+    }
+  
+    refill() {
+      this.inkLevel = 100;
+    }
+  }
+  
+  const txtItem = document.querySelector(".textarea__item");
+  const txtCounter = document.querySelector(".textarea__counter span");
+  const refillBtn = document.querySelector(".refill-btn");
+  const colorPicker = document.querySelector("#marker-color-picker");
+  
+  // set initial color
+  const initialColor = colorPicker.value;
+  const myMarker = new Marker(initialColor, 100);
+  txtItem.style.color = myMarker.color;
+  
+  txtCounter.innerHTML = txtItem.getAttribute("maxlength");
+  txtItem.addEventListener("keyup", function(e) {
+    if (e.code !== "Space") {
+      const txtCounterResult = txtItem.getAttribute("maxlength") - txtItem.value.length;
+      txtCounter.innerHTML = txtCounterResult;
+      const inkUsed = (txtItem.value.replace(/\s/g, "").length) * 0.5;
+      if (inkUsed > myMarker.inkLevel) {
+        alert("Маркер закінчився!");
+        txtItem.disabled = true;
+        refillBtn.disabled = false;
+      } else {
+        myMarker.inkLevel -= inkUsed;
+        alert(`Ink level: ${myMarker.inkLevel}%`);
+        txtItem.style.color = myMarker.color;
+      }
+    }
+  });
+  
+  colorPicker.addEventListener("change", function() {
+    myMarker.color = colorPicker.value;
+    txtItem.style.color = myMarker.color;
+  });
+  
+  refillBtn.addEventListener("click", function() {
+    myMarker.refill();
+    alert("Маркер перезаповнено");
+    txtItem.disabled = false;
+    txtItem.value = "";
+    txtCounter.innerHTML = txtItemLimit;
+    txtItem.style.color = myMarker.color;
+  });
